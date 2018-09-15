2a4abce976dc48a6ab177e27ec7da99737651458
JaseG256 <jason_r_gibbs@yahoo.com>
2018-09-15 12:54:59 -0400

    Button text output displaying on screen (processnumPad)

--- CalculatorTwo/src/sample/Controller.java
+++ CalculatorTwo/src/sample/Controller.java
@@ -2,7 +2,9 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.text.Text;
+
 
 
 public class Controller {
@@ -12,6 +14,12 @@ public class Controller {
 
     @FXML
     private void processNumPad(ActionEvent event) {
+        String value = ((Button)event.getSource()).getText();
+        output.setText(output.getText() + value);
+    }
+
+    @FXML
+    private void processOperator(ActionEvent event) {
 
     }
 }
