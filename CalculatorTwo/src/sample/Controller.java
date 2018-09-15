cacfc52374601ec3b3ca52c5244f3adb118cb346
JaseG256 <jason_r_gibbs@yahoo.com>
2018-09-15 12:36:39 -0400

    Button rows added in view

--- CalculatorTwo/src/sample/Controller.java
+++ CalculatorTwo/src/sample/Controller.java
@@ -1,4 +1,17 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+
 public class Controller {
+
+    @FXML
+    private Text output;
+
+    @FXML
+    private void processNumPad(ActionEvent event) {
+
+    }
 }
