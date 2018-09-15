e9fc77b7e754684bcda9e3c34f58aaab02592b4e
JaseG256 <jason_r_gibbs@yahoo.com>
2018-09-15 13:01:52 -0400

    Operator defined as a String value

--- CalculatorTwo/src/sample/Controller.java
+++ CalculatorTwo/src/sample/Controller.java
@@ -12,6 +12,9 @@ public class Controller {
     @FXML
     private Text output;
 
+    // Should be converted to Enum defining operator symbols i.e. (+,-,*,/ etc)
+    private String operator;
+
     @FXML
     private void processNumPad(ActionEvent event) {
         String value = ((Button)event.getSource()).getText();
@@ -20,6 +23,10 @@ public class Controller {
 
     @FXML
     private void processOperator(ActionEvent event) {
+        String value = ((Button)event.getSource()).getText();
 
+        if (!"=".equals(value)) {
+
+        }
     }
 }
